c0fbec0a8e65e7195f23e2d696f899d7ad61a5eb
mohamed shames <mohamedhames900@gmail.com>
2023-03-08 21:13:52 +0200

    update

--- script/script.js
+++ script/script.js
@@ -8,3 +8,21 @@ menu.addEventListener("click", () =>{
 window.onscroll = () => {
     navBer.classList.remove("active");
 }
+
+let dropdown_items = document.querySelectorAll(".items");
+
+dropdown_items.forEach(items => {
+    items.onclick = () => {
+        items_parent = items.parentElement.parentElement;
+        const output = items_parent.querySelector(".output");
+        output.value = items.innerText;
+    }
+})
+
+const view_Page = document.querySelectorAll(".view")
+
+view_Page.forEach(item => {
+    item.onclick = () => {
+        location.href = "../components/view_company.html"
+    }
+})
